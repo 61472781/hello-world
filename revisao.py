7ec2a71284971d1ebd857f170cf25df4fd32dffc
61472781 <vanessabarbosamendonca@gmail.com>
2025-05-07 15:37:53 -0300

    exemplo de comparaçaõ

---
+++ revisao.py
@@ -0,0 +1,27 @@
+#comentário
+
+#variavel é um espaço na memória que receberá dado
+#tipo : texto, número (int e float (moeda,altura,peso))
+
+#exemplo de número float (flutuante)
+peso = 89.5
+print (peso)
+ 
+ #exemplo de número inteiro
+ iade = 18
+  print (idade)
+
+ #exemplo de variavel do tipo texto
+ nome = ("maria")
+ sobrenome = "silva"
+  print(nome,sobrenome)
+
+#exemplo se A é maior que B
+a = 5
+b = 10
+
+if a > b:
+        print ( 'A é maior que B')
+else
+        print('A é MENOR  que B')
+    
